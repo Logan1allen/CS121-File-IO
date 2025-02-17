6470ae724f02ad4ee2953bd658e0c1c0ed760874
Logan1allen <logan.allen@bsu.edu>
2025-02-17 01:45:56 +0000

    final verison

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # CS121-File-IO
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+create int main
+    call all varible that I will need
+    std::ifstream inFile;
+    std::string currentLine;
+    std::stringstream converter;
+    std::stringstream ss;
+    int num1, num2, sum;
+    std::string sNum1, sNum2, text;
+
+    open the file data.csv
+
+    use a while loop called getline with the parameters infile and currentline
+        clear the stringStream
+        clear the converter
+
+        string stream currentline
+        define the setup/ or sort of layout of the data in the file
+        getline(ss, sNum1, ',')
+        getline(ss, sNum2, ',')
+        getline(ss, text)
+
+        convert sNum1 to num1
+        clear the converter
+
+        convert sNum2 to num2
+        clear the converter
+
+        create sum which adds ints num1 and num2
+        create a forloop that loops for sum amount of times
+        it will be used to print out the text the correct number of times
+
+        end main
--- main.cpp
+++ main.cpp
@@ -8,36 +8,36 @@ int main(){
     std::string currentLine;
     std::stringstream converter;
     std::stringstream ss;
-    int counter;
-    std::string sCounter;
-    std::string text;
+    int num1, num2, sum;
+    std::string sNum1, sNum2, text;
 
     inFile.open("data.csv");
-  while (getline(inFile, currentLine)){
-    //temporary stream for parsing commas
 
-    //clear the stringstreams
-    ss.clear();
-    ss.str("");
-    converter.clear();
-    converter.str("");
+    while (getline(inFile, currentLine)) {
+        ss.clear();
+        ss.str("");
+        converter.clear();
+        converter.str("");
 
-    ss.str(currentLine);
-    std::cout << currentLine << std::endl;
+        ss.str(currentLine);
+        getline(ss, sNum1, ',');
+        getline(ss, sNum2, ',');
+        getline(ss, text);
 
-    getline(ss, sCounter, ',');
+        converter << sNum1;
+        converter >> num1;
+        converter.clear();
+        converter.str("");
 
-    //read text, newline is default delimiter
-    getline(ss, text);
+        converter << sNum2;
+        converter >> num2;
+        
+        sum = num1 + num2;
+        for (int i = 0; i < sum; i++) {
+            std::cout << text << " ";
+        }
+        std::cout << std::endl;
+    }
 
-    // only counter needs to be converted, so send it
-    // to converter stream
-    converter << sCounter;
-    converter >> counter;
-
-    std::cout << counter << " " << text << std::endl;
-
-
-  }
-  inFile.close();
+    inFile.close();
 } // end main
